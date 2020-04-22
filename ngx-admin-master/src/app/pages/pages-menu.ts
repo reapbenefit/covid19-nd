6049725f85180af900a01d5223c9b6fc388ee98d
mihsolan <64087674+mihsolan@users.noreply.github.com>
2020-04-22 11:25:05 +0530

    restructured base project

---
+++ ngx-admin-master/src/app/pages/pages-menu.ts
@@ -0,0 +1,16 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: 'Menu 1',
+    icon: 'shopping-cart-outline',
+    link: '/pages/dashboard',
+    home: true,
+  },
+  {
+    title: 'Menu 2',
+    icon: 'home-outline',
+    link: '/pages/iot-dashboard',
+  },
+ 
+];
