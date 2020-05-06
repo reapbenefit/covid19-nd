7b42958ec52cec2f2b2db591fda626c4ce9a6447
mihsolan <64087674+mihsolan@users.noreply.github.com>
2020-05-06 21:57:18 +0530

    added line graph to landing page

--- ngx-admin-master/src/app/pages/e-commerce/e-commerce.component.ts
+++ ngx-admin-master/src/app/pages/e-commerce/e-commerce.component.ts
@@ -1,8 +1,68 @@
 import { Component } from '@angular/core';
-
+import { NbThemeService, NbColorHelper } from '@nebular/theme';
 @Component({
   selector: 'ngx-ecommerce',
   templateUrl: './e-commerce.component.html',
 })
 export class ECommerceComponent {
+  data: any;
+  options: any;
+  themeSubscription: any;
+  constructor(private theme: NbThemeService) {
+    this.themeSubscription = this.theme.getJsTheme().subscribe(config => {
+
+      const colors: any = config.variables;
+      const chartjs: any = config.variables.chartjs;
+
+      this.data = {
+        labels: ['1', '2', '3', '4', '5', '6', '7'],
+        datasets: [{
+          data: [65, 59, 80, 81, 56, 55, 40],
+          label: 'Completed',
+          backgroundColor: NbColorHelper.hexToRgbA(colors.primary, 0.3),
+          borderColor: colors.primary,
+        }, {
+          data: [28, 48, 40, 19, 56, 27, 40],
+          label: 'Pending',
+          backgroundColor: NbColorHelper.hexToRgbA(colors.danger, 0.3),
+          borderColor: colors.danger,
+        }, 
+        ],
+      };
+
+      this.options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [
+            {
+              gridLines: {
+                display: true,
+                color: chartjs.axisLineColor,
+              },
+              ticks: {
+                fontColor: chartjs.textColor,
+              },
+            },
+          ],
+          yAxes: [
+            {
+              gridLines: {
+                display: true,
+                color: chartjs.axisLineColor,
+              },
+              ticks: {
+                fontColor: chartjs.textColor,
+              },
+            },
+          ],
+        },
+        legend: {
+          labels: {
+            fontColor: chartjs.textColor,
+          },
+        },
+      };
+    });
+  }
 }
